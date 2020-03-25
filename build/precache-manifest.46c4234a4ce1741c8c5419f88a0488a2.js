883deea1cb5f1bb53703366d0d022675439bd043
vaasu <mohiit1502@gmail.com>
2020-03-26 02:17:11 +0530

    Deploying built boilerplate for gh-pages

---
+++ build/precache-manifest.46c4234a4ce1741c8c5419f88a0488a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ed38c3a05b7de86b240ca2e7302d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "523712a40bddd2346aa2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c53e76bd3597afcea6bd",
+    "url": "/static/js/2.09de4f42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09de4f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523712a40bddd2346aa2",
+    "url": "/static/js/main.9c54a2c4.chunk.js"
+  },
+  {
+    "revision": "072fa0f922bc3d3bd303",
+    "url": "/static/js/runtime-main.d43e8b39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
